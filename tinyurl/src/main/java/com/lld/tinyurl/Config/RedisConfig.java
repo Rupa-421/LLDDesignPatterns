1209d0a2f550ebfd06b6265dd701857e5ad4902b
Rupa-421 <rupasrikkd147@gmail.com>
2025-10-11 09:49:42 +0530

    Adding config of redis

---
+++ tinyurl/src/main/java/com/lld/tinyurl/Config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.lld.tinyurl.Config;
+
+
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration(){
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(1))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .disableCachingNullValues();
+    }
+}
